6472f33e3cc38335ae4805290cba7310bfd02ea7
Manta <32708404+Soecka@users.noreply.github.com>
2024-12-03 21:17:20 +0800

    wrangler，lark client and github action (#1)

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { LarkApp } from './lark';
+
+export interface Env
+    extends Record<
+        | 'BUCKET_BASE_URL'
+        | 'LARK_HOST'
+        | 'LARK_ID'
+        | 'LARK_SECRET'
+        | 'LARK_UPLOAD_KEY',
+        string
+    > {
+    R2_BUCKET: R2Bucket;
+}
+
+const larkClient = new LarkApp();
+
+async function fetch(request: Request, env: Env) {
+    if (request.method !== 'PUT' && request.method !== 'POST')
+        return new Response(`Unsupported method`, { status: 405 });
+
+    const url = new URL(request.url);
+
+    const {
+        LARK_ID,
+        LARK_SECRET,
+        LARK_HOST,
+        LARK_UPLOAD_KEY,
+        R2_BUCKET,
+        BUCKET_BASE_URL
+    } = env;
+
+    /**
+     * @param {string} AUTH_HEADER_KEY Custom header to check for key
+     */
+    const AUTH_HEADER_KEY = 'X-Lark-Upload-Token',
+        passKey = request.headers.get(AUTH_HEADER_KEY);
+
+    /**
+     * Upload file from lark to r2
+     */
+    if (passKey !== LARK_UPLOAD_KEY)
+        return new Response(`Invalid upload token`, { status: 401 });
+
+    const searchParams = Object.fromEntries([...url.searchParams]);
+
+    /**
+     * @param {string} file_token File token of lark image
+     * @param {string} name Name of the file
+     * @param {string} type Type of the file (image)
+     */
+    const { file_token = '', type = '' } = searchParams;
+
+    if (larkClient.id !== LARK_ID)
+        larkClient.init({
+            id: LARK_ID,
+            secret: LARK_SECRET,
+            host: LARK_HOST
+        });
+
+    const file_token_array = file_token.split(','),
+        type_array = type.split(',');
+
+    const uploadPromises = file_token_array.map(async (file_token, index) => {
+        const file = await larkClient.downloadFile(file_token);
+        const key = `${file_token}.${type_array[index].split('/')[1]}`;
+
+        await R2_BUCKET.put(key, file, {
+            httpMetadata: request.headers
+        });
+
+        return `${BUCKET_BASE_URL}/${key}`;
+    });
+
+    const files = await Promise.all(uploadPromises);
+
+    return new Response(JSON.stringify({ files }), {
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
+
+export default { fetch } satisfies ExportedHandler<Env>;
---
+++ src/lark.ts
@@ -0,0 +1,90 @@
+export type LarkAppOption = Record<'host' | 'id' | 'secret', string>;
+export interface TenantToken {
+    expire: number;
+    tenant_access_token: string;
+}
+
+function cache<I, O>(
+    executor: (this: LarkApp, cleaner: () => void, ...data: I[]) => O,
+    title: string
+) {
+    let cacheData: O | undefined;
+
+    return function (this: LarkApp, ...data: I[]) {
+        if (cacheData != null) return cacheData;
+
+        console.trace(`[Cache] execute: ${title}`);
+
+        cacheData = executor.call(
+            this,
+            (): void => (cacheData = undefined),
+            ...data
+        );
+        Promise.resolve(cacheData).then(
+            data => console.log(`[Cache] refreshed: ${title} => ${data}`),
+            error => console.error(`[Cache] failed: ${error?.message || error}`)
+        );
+        return cacheData;
+    };
+}
+
+export class LarkApp implements LarkAppOption {
+    host = '';
+    secret = '';
+    id = '';
+
+    accessToken?: string;
+
+    init({ host, id, secret }: LarkAppOption) {
+        console.assert(
+            'window' in globalThis,
+            "App Secret can't be used in client"
+        );
+
+        this.host = host;
+        this.id = id;
+        this.secret = secret;
+    }
+
+    getTenantAccessToken = cache(async clean => {
+        const { id, secret } = this;
+
+        console.assert(
+            Boolean(id && secret),
+            'Id & Secret of Lark App are required'
+        );
+
+        const response = await fetch(
+            new URL('auth/v3/tenant_access_token/internal', this.host),
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ app_id: id, app_secret: secret })
+            }
+        );
+
+        const body: TenantToken = await response.json();
+
+        setTimeout(() => {
+            delete this.accessToken;
+            clean();
+        }, body!.expire * 1000);
+
+        return (this.accessToken = body!.tenant_access_token);
+    }, 'Tenant Access Token');
+
+    getAccessToken() {
+        return this.getTenantAccessToken();
+    }
+
+    async downloadFile(id: string) {
+        await this.getAccessToken();
+
+        const { body } = await fetch(
+            `${this.host}drive/v1/medias/${id}/download`,
+            { headers: { Authorization: `Bearer ${this.accessToken}` } }
+        );
+
+        return body!;
+    }
+}
